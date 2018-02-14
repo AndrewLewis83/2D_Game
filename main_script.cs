0f38993432b769320089f79081235c02b7b4f02a
dookadoo <35851914+dookadoo@users.noreply.github.com>
2018-02-14 12:12:17 -0700

    Add files via upload

---
+++ main_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using map_grid_script.cs
+
+public class main_script.cs : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		grid_initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
