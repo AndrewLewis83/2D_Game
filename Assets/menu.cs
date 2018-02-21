4f2c80c1197e0b07c648f175dcb22d8214cdb782
Andy <ajlewis1@me.com>
2018-02-21 10:57:52 -0700

    Menu update

---
+++ Assets/menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame () {
+        //SceneManager.LoadScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void quitButton(){
+        Debug.Log("Application quit");
+        Application.Quit();
+    }
+}
