de76e8c34eef73197d57fc31bc81de9cf1bd0f4e
dookadoo <35851914+dookadoo@users.noreply.github.com>
2018-02-22 13:30:45 -0700

    still trying to get sprite rendering working

--- Assets/map_grid_script.cs
+++ Assets/map_grid_script.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum TILE_TEXTURE {//enumerates and assigns numeric value to identify which textures to render
-    GRASS1_TILE_TEXTURE,
-    GRASS2_TILE_TEXTURE,
-    GRASS3_TILE_TEXTURE,
-    GRASS4_TILE_TEXTURE,
-	WALL1_TILE_TEXTURE,
-	WALL2_TILE_TEXTURE,
-	WATER1_TILE_TEXTURE,
-    WATER2_TILE_TEXTURE,
-    WATER3_TILE_TEXTURE,
-    WATER4_TILE_TEXTURE,
-    WATER5_TILE_TEXTURE,
-    WATER6_TILE_TEXTURE,
-    WATER7_TILE_TEXTURE,
-    WATER8_TILE_TEXTURE,
-    WATER9_TILE_TEXTURE,
+    GRASS1_TILE_TEXTURE, //0
+    GRASS2_TILE_TEXTURE, //1
+    GRASS3_TILE_TEXTURE, //2
+    GRASS4_TILE_TEXTURE, //3
+	WALL1_TILE_TEXTURE,  //4
+	WALL2_TILE_TEXTURE,  //5
+	WATER1_TILE_TEXTURE, //6
+    WATER2_TILE_TEXTURE, //7
+    WATER3_TILE_TEXTURE, //8
+    WATER4_TILE_TEXTURE, //9
+    WATER5_TILE_TEXTURE, //10
+    WATER6_TILE_TEXTURE, //11
+    WATER7_TILE_TEXTURE, //12
+    WATER8_TILE_TEXTURE, //13
+    WATER9_TILE_TEXTURE, //14
     MAX_TILE_TEXTURE
 }
 
@@ -27,8 +27,7 @@ public class grid_node{					//the grid is made up of these nodes
 	public bool is_occupied;		//bool value which determines wether the spot is occupied
 	public TILE_TEXTURE texture;				//numeric value which determines textures
 	public GameObject tile;
-	
-	//public character* occupant;	//pointer to current occupant if is_occupied is true
+	public Sprite [] image = new Sprite[(int)TILE_TEXTURE.MAX_TILE_TEXTURE];
 
 	public grid_node(int _x, int _y, bool _is_occupied, TILE_TEXTURE _texture){
 		x_pos = _x;
@@ -38,6 +37,7 @@ public class grid_node{					//the grid is made up of these nodes
 			tile = new GameObject("tile");//new Vector3(x_pos * 16.0F, y_pos * 16.0F, 0), Quaternion.identity)
 			tile.transform.Translate(x_pos * 16.0F, y_pos * 16.0F, 0);
 			//tile.AddComponent<SpriteRenderer>() as SpriteRenderer;
+		
 	}
 }
 
