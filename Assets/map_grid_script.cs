39d96e7db137e56ee202a852e7da571450b302c2
dookadoo <35851914+dookadoo@users.noreply.github.com>
2018-02-21 21:43:15 -0700

    Added grid rendering to scene
    i got the grid to render as game objects but I have to attach sprites to the game objects dynamically still

--- Assets/map_grid_script.cs
+++ Assets/map_grid_script.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum TILE_TEXTURE : int{//enumerates and assigns numeric value to identify which textures to render
+public enum TILE_TEXTURE {//enumerates and assigns numeric value to identify which textures to render
     GRASS1_TILE_TEXTURE,
     GRASS2_TILE_TEXTURE,
     GRASS3_TILE_TEXTURE,
@@ -21,26 +21,35 @@ enum TILE_TEXTURE : int{//enumerates and assigns numeric value to identify which
     MAX_TILE_TEXTURE
 }
 
-struct grid_node{					//the grid is made up of these nodes
+public class grid_node{					//the grid is made up of these nodes
 	public int x_pos;				//node position
 	public int y_pos;
 	public bool is_occupied;		//bool value which determines wether the spot is occupied
-	public int TEXTURE;				//numeric value which determines textures
-	//public character* occupant;	//pointer to current occupant if is_occupied is true
+	public TILE_TEXTURE texture;				//numeric value which determines textures
 	public GameObject tile;
+	
+	//public character* occupant;	//pointer to current occupant if is_occupied is true
+
+	public grid_node(int _x, int _y, bool _is_occupied, TILE_TEXTURE _texture){
+		x_pos = _x;
+		y_pos = _y;
+		is_occupied = _is_occupied;
+		texture = _texture;
+			tile = new GameObject("tile");//new Vector3(x_pos * 16.0F, y_pos * 16.0F, 0), Quaternion.identity)
+			tile.transform.Translate(x_pos * 16.0F, y_pos * 16.0F, 0);
+			//tile.AddComponent<SpriteRenderer>() as SpriteRenderer;
+	}
 }
 
+
 public class map_grid_script : MonoBehaviour { 
-    grid_node [,] GRID = new grid_node[24, 24];
+    grid_node [,] GRID = new grid_node[25, 25];
+	
 
     void grid_initialize(){			//function that is called at run time to determine contents of grid
-		for(int i=0; i<24; i++){	
-			for(int j=0; j<24; j++){
-				GRID[i, j].x_pos = i;
-				GRID[i, j].y_pos = j;
-				GRID[i, j].is_occupied = false;
-				GRID[i, j].TEXTURE = (int)TILE_TEXTURE.GRASS1_TILE_TEXTURE;
-				Instantiate(GRID[i, j].tile);
+		for(int i=0; i<25; i++){	
+			for(int j=0; j<25; j++){
+				GRID[i, j] = new grid_node(i, j, false, TILE_TEXTURE.GRASS1_TILE_TEXTURE);
 			}
 		}
 	}
@@ -55,5 +64,13 @@ public class map_grid_script : MonoBehaviour {
 		}else	if(GRID[x, y].is_occupied == false){
 				return true;
 			}else {return false;}
+	}
+	
+	void Start () {
+		grid_initialize();
+	}
+	
+	void Update () {
+		
 	}
 }
