d7d65f8381a7cbe3ebf78bae29291c7dc08797d4
dookadoo <35851914+dookadoo@users.noreply.github.com>
2018-02-28 10:46:13 -0700

    added cursor movement

---
+++ cursor_script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor_script : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newxPos;
+		float newyPos;
+		float gridl = .16f;
+			if(Input.GetKeyDown("w")){
+				newxPos = transform.position.x;
+				newyPos = transform.position.y + gridl;
+				transform.position = new Vector3(newxPos, newyPos, 0);
+			}else if(Input.GetKeyDown("a")){
+				newxPos = transform.position.x - gridl;
+				newyPos = transform.position.y;
+				transform.position = new Vector3(newxPos, newyPos, 0);
+			}else if(Input.GetKeyDown("s")){
+				newxPos = transform.position.x;
+				newyPos = transform.position.y - gridl;
+				transform.position = new Vector3(newxPos, newyPos, 0);
+			}else if(Input.GetKeyDown("d")){
+				newxPos = transform.position.x + gridl;
+				newyPos = transform.position.y;
+				transform.position = new Vector3(newxPos, newyPos, 0);
+			}else if(Input.GetKeyDown("Enter")){
+				
+			}else if(Input.GetKeyDown("Shift")){
+				
+			}
+	}
+}
