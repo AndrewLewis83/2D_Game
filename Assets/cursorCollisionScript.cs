eb188f285e5672954785b9980ab459f1a62ce5f8
Andy <ajlewis1@me.com>
2018-03-02 20:24:37 -0700

    Enemy collision added. Enemies now trigger response in cursor.

---
+++ Assets/cursorCollisionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorCollisionScript : MonoBehaviour{
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "character")
+        {
+            print("Collision detected with trigger object " + other.name);
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        print("Still colliding with trigger object " + other.name);
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
+    }
+}
